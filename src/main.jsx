46dbf2eade23a84856abd3727d0c52e341e4775c
David Konsumer <konsumer@jetboystudio.com>
2026-03-18 01:48:17 -0700

    simplify redirect

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,9 @@ import "./index.css";
 import App from "./App.jsx";
 
 if (window?.location?.hash?.startsWith("#api_key=")) {
-  localStorage.polykey = window.location.hash.replace("#api_key=", "");
-  window.location = window.location
-    .toString()
-    .replace(window.location.hash, "");
+  const hash = window.location.hash.toString();
+  localStorage.polykey = hash.replace("#api_key=", "");
+  window.location = window.location.toString().replace(hash, "");
 }
 
 createRoot(document.getElementById("root")).render(
